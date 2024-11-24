a1773aea1a7c0070d633963a479cb125b5cfd37d
PawelDrozdowski <pawel.drozdowski.555@gmail.com>
2024-11-24 11:28:01 +0100

    HtmlEncode on user input
    sql injection already prevented

--- NET MVC LeetSpeak Text Generator/Controllers/ApiCallsControllerApi.cs
+++ NET MVC LeetSpeak Text Generator/Controllers/ApiCallsControllerApi.cs
@@ -1,19 +1,11 @@
 ﻿using AutoMapper;
-using Azure;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 using NET_MVC_LeetSpeak_Text_Generator.Contracts;
 using NET_MVC_LeetSpeak_Text_Generator.Data;
-using NET_MVC_LeetSpeak_Text_Generator.Helpers;
 using NET_MVC_LeetSpeak_Text_Generator.Models;
 using NET_MVC_LeetSpeak_Text_Generator.Models.DTO;
-using NuGet.Common;
-using System.Drawing.Drawing2D;
-using System.Net.Http.Headers;
-using System.Security.Policy;
-using System.Text;
+using System.Web;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -39,7 +31,7 @@ namespace NET_MVC_LeetSpeak_Text_Generator.Controllers
         [HttpGet]
         public async Task<ActionResult> Get()
         {
-            if(User.Identity.IsAuthenticated)
+            if (User.Identity.IsAuthenticated)
                 return Ok(await _context.ApiCalls.ToListAsync());
             return Unauthorized();
         }
@@ -52,21 +44,23 @@ namespace NET_MVC_LeetSpeak_Text_Generator.Controllers
             var apiCall = _mapper.Map<ApiCall>(apiCallDto);
             if (ModelState.IsValid)
             {
+                string translated;
                 try
                 {
                     TranslatorDtoResponse response = await _translator.TranslateText(apiCallDto.Request);
-                    apiCall.Response = response.contents.translated;
+                    translated = response.contents.translated;
                 }
                 catch (Exception ex)
                 {
                     _logger.LogInformation($"Api call failure {apiCallDto.Request}, {ex.Message}");
                     return Problem("API Error");
                 }
-
+                apiCall.Request = HttpUtility.HtmlEncode(apiCall.Request);
+                apiCall.Response = HttpUtility.HtmlEncode(translated);
                 _context.Add(apiCall);
                 await _context.SaveChangesAsync();
                 _logger.LogInformation($"Api call success {apiCallDto.Request}");
-                return Ok(apiCall.Response);
+                return Ok(translated);
             }
             return BadRequest();
         }
